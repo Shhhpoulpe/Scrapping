625053d99b291fda204ed08ec9ad5daee5870429
Shhhpoulpe <max.jobard@hotmail.fr>
2021-12-15 11:43:31 +0100

    skip consent

--- selenium/youtubescrapper.py
+++ selenium/youtubescrapper.py
@@ -5,5 +5,5 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('/Users/maxime/Desktop/Code/Scrapping/selenium/chromedriver')
 driver.get('https://www.youtube.com/c/TurboA/videos')
-WebDriverWait(driver,20).until(EC.frame_to_be_available_and_switch_to_it((By.ID,"yDmH0d")))
-driver.find_element_by_xpath("//*[contains(text(), 'J'ACCEPTE')]").click()
+WebDriverWait(driver,20)
+driver.find_elements_by_xpath(f"//*[contains(text(), 'accepte')]")[-1].click()
