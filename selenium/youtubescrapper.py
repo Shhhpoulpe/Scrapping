f5e8e9f8e332fda382d3f73e1e729093264ed783
Shhhpoulpe <max.jobard@hotmail.fr>
2021-12-16 15:17:29 +0100

    clean du code

--- selenium/youtubescrapper.py
+++ selenium/youtubescrapper.py
@@ -10,40 +10,44 @@ from discord.ext import tasks
 from discord.ext import commands
 from discord_param import discord_token
 
+# Création de PoulpoTron3.0
 client = discord.Client()
 
-@tasks.loop(seconds = 10) # repeat after every 10 seconds
+# Création de l'objet selenium headless
+ser = Service(r"C:\Users\maxjo\OneDrive\Bureau\Code\Scrapping\selenium\chromedriver.exe")
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(service = ser, options=options)
+
+# Connect to Youtube and accept the cookies
+driver.get('https://www.youtube.com/channel/UCJLvLnSLEoJEMUrMMqNEomQ/videos')
+WebDriverWait(driver,20)
+driver.find_elements(By.XPATH,"//*[contains(text(), 'accepte')]")[-1].click()
+
+# Boucle discord de 10 secondes
+@tasks.loop(seconds = 10)
 async def myLoop():
-    # Cherche la dernière vidéo
+    # Attends que le bot soit connecté
     await client.wait_until_ready()
     new_video = findLastVideo()
     global last_video
     channel = client.get_channel(741298761569009826)
+    # Envoie un message si la dernière vidéo est différente de la dernière vidéo envoyée
     if new_video != last_video:
         print(new_video)
         last_video = new_video
-        # Send a message to the channel
         await channel.send(new_video)
     else:
         print("No new video")
-        await channel.send("No new video")
 
+# Cherche la dernière vidéo
 def findLastVideo():
     driver.refresh()
     time.sleep(10)
     WebDriverWait(driver,20)
     last_video = driver.find_element(By.ID,"video-title")
     return last_video.text
-
-# Connect to Youtube and accept the cookies
-ser = Service(r"C:\Users\maxjo\OneDrive\Bureau\Code\Scrapping\selenium\chromedriver.exe")
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(service = ser, options=options)
-driver.get('https://www.youtube.com/channel/UCJLvLnSLEoJEMUrMMqNEomQ/videos')
-WebDriverWait(driver,20)
-driver.find_elements(By.XPATH,"//*[contains(text(), 'accepte')]")[-1].click()
 
 # Cherche la dernière vidéo
 last_video = findLastVideo()
